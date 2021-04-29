9e6bfb8c743d00c6a8aee926cb8bd4b943dc97f8
Hiruni06 <56539533+Hiruni06@users.noreply.github.com>
2021-04-29 10:04:36 +0530

    basic

---
+++ home.php
@@ -0,0 +1,17 @@
+<html lang = 'en'>
+<head>
+
+<title> Home Page </title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="/FitnessZone/styles/home.css">
+</head>
+
+<body>
+<?php include 'navbar.php';?>
+<img src = "/FitnessZone/images/logo.jpg" style = width:100% >
+<div class = "text">
+<h1>No Pain, No Gain</h1>
+</div>
+</body>
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/FitnessZone/styles/navbar.css">
+</head>
+
+<body>
+    <div class = "topnav">
+
+    <a href="/thoga.lk/farmer/dash" class="navlogo"><img width=80px src="/FitnessZone/images/logo2.jpg" alt="" class="logo" ></a>
+    <a href = ""> Add User </a>
+    <a href = ""> View User </a>
+    <a href = ""> Edit User </a>
+
+
+    </div>
+
+    <div class = "nav-right">
+        <a href =""> Logout</a>
+    </div>
+</body>
+
+
+</html>
